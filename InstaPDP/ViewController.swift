23c38dc078b7ea49d445930ca2906facc51a243b
Khurshid <shamsun.com@gmail.com>
2020-09-20 11:31:38 +0900

    Initial Commit

---
+++ InstaPDP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaPDP
+//
+//  Created by KHURSHIDBEK on 2020/09/20.
+//  Copyright © 2020 MySoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
